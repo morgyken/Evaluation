38427c3dfc4bc1d9124e4f326148c47f4125815b
samueldervis <sodhiambo@collabmed.com>
2016-12-10 17:56:26 +0300

    Visit destination and macros

--- Database/Migrations/2016_07_26_143016_create_patient_visits_table.php
+++ Database/Migrations/2016_07_26_143016_create_patient_visits_table.php
@@ -16,24 +16,6 @@ class CreatePatientVisitsTable extends Migration {
             $column->integer('clinic')->unsigned();
             $column->integer('patient')->unsigned();
             $column->integer('purpose')->nullable()->unsigned();
-            $column->integer('destination')->nullable()->unsigned();
-            //module details
-            $column->boolean('nurse')->default(false);
-            $column->dateTime('nurse_out')->nullable();
-            $column->boolean('theatre')->default(false);
-            $column->dateTime('theatre_out')->nullable();
-            $column->boolean('diagnostics')->default(false);
-            $column->dateTime('diagnostics_out')->nullable();
-            $column->boolean('evaluation')->default(false);
-            $column->dateTime('evaluation_out')->nullable();
-            $column->boolean('laboratory')->default(false);
-            $column->dateTime('laboratory_out')->nullable();
-            $column->boolean('radiology')->default(false);
-            $column->dateTime('radiology_out')->nullable();
-            $column->boolean('pharmacy')->default(false);
-            $column->dateTime('pharmacy_out')->nullable();
-            $column->boolean('optical')->default(false);
-            $column->dateTime('optical_out')->nullable();
             //payments
             $column->integer('user')->unsigned();
             $column->string('payment_mode')->default('cash');
@@ -46,9 +28,7 @@ class CreatePatientVisitsTable extends Migration {
             $column->foreign('user')->references('id')->on('users')
                     ->onUpdate('cascade')
                     ->onDelete('cascade');
-            $column->foreign('destination')->references('id')->on('users')
-                    ->onUpdate('cascade')
-                    ->onDelete('cascade');
+
             $column->foreign('patient')
                     ->references('id')
                     ->on('reception_patients')
---
+++ Database/Migrations/2016_07_26_143114_create_visit_destinations_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVisitDestinationsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('evaluation_visit_destinations', function (Blueprint $column) {
+            $column->increments('id');
+            $column->integer('visit')->unsigned();
+            $column->integer('user')->unsigned();
+            $column->integer('destination')->unsigned()->nullable();
+            /*
+             * Department can be :
+             * - nurse
+             * - doctor
+             * - theatre
+             * - diagnostics
+             * - lab
+             * - radiology
+             * - pharmacy
+             * - optical
+             */
+            $column->string('department');
+            $column->boolean('checkout')->default(false);
+            $column->dateTime('begin_at')->nullable();
+            $column->dateTime('finish_at')->nullable();
+            $column->softDeletes();
+            $column->timestamps();
+            $column->foreign('visit')
+                    ->references('id')
+                    ->on('evaluation_visits')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $column->foreign('user')->references('id')->on('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $column->foreign('destination')->references('id')->on('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('evaluation_visit_destinations');
+    }
+
+}
