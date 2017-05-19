59f464f88ccff6ea0ee772a9dad7a31c0c3b755e
bravo kiptoo <bkiptoo@collabmed.com>
2017-05-19 14:07:40 +0300

    try

--- Resources/views/partials/labs/with_titles.blade.php
+++ Resources/views/partials/labs/with_titles.blade.php
@@ -7,19 +7,27 @@
     <tr>
         <th colspan="2" style="background:#9999CC">{{$title->name}} ({{$tests->count()}})</th>
     </tr>
-    @foreach($tests as $test)
-    <tr>
-        <td>
-            {{$test->subtests->name}}
-            <input type="hidden" name="item{{$item->id}}" value="{{$item->id}}" />
-            <input type="hidden" name="test{{$item->id}}[]" value="{{$test->subtest}}" />
-        </td>
-        <td>
-            <?php $type = $test->subtests->this_test->lab_result_type; ?>
-            @include('evaluation::partials.labs.input_field')
-        </td>
-    </tr>
-    @endforeach
+    <?php
+    try {
+        ?>
+        @foreach($tests as $test)
+        <tr>
+            <td>
+                {{$test->subtests->name}}
+                <input type="hidden" name="item{{$item->id}}" value="{{$item->id}}" />
+                <input type="hidden" name="test{{$item->id}}[]" value="{{$test->subtest}}" />
+            </td>
+            <td>
+                <?php $type = $test->subtests->this_test->lab_result_type; ?>
+                @include('evaluation::partials.labs.input_field')
+            </td>
+        </tr>
+        @endforeach
+        <?php
+    } catch (\Exception $e) {
+        
+    }
+    ?>
     @endforeach
     <tr>
         <td colspan="2">
--- Resources/views/partials/labs/without_titles.blade.php
+++ Resources/views/partials/labs/without_titles.blade.php
@@ -1,23 +1,29 @@
 <table class="table table-condensed table-striped">
     <?php
     $tests = get_lab_template($item->procedures->id);
+    try {
+        ?>
+        @foreach($tests as $test)
+        <tr>
+            <td>
+                {{$test->subtests->name}}
+                <input type="hidden" name="item{{$item->id}}" value="{{$item->id}}" />
+                <input type="hidden" name="test{{$item->id}}[]" value="{{$test->subtest}}" />
+            </td>
+            <td>
+                <?php $type = $test->subtests->this_test->lab_result_type; ?>
+                @include('evaluation::partials.labs.input_field')
+            </td>
+        </tr>
+        @endforeach
+        <tr>
+            <td colspan="2">
+                @include('evaluation::partials.labs.comment')
+            </td>
+        </tr>
+        <?php
+    } catch (\Ecxeption $e) {
+        
+    }
     ?>
-    @foreach($tests as $test)
-    <tr>
-        <td>
-            {{$test->subtests->name}}
-            <input type="hidden" name="item{{$item->id}}" value="{{$item->id}}" />
-            <input type="hidden" name="test{{$item->id}}[]" value="{{$test->subtest}}" />
-        </td>
-        <td>
-            <?php $type = $test->subtests->this_test->lab_result_type; ?>
-            @include('evaluation::partials.labs.input_field')
-        </td>
-    </tr>
-    @endforeach
-    <tr>
-        <td colspan="2">
-            @include('evaluation::partials.labs.comment')
-        </td>
-    </tr>
 </table>
