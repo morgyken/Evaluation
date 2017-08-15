789a253aef1669413b8cdae15df301a46a4bbd4f
bravo kiptoo <bkiptoo@collabmed.com>
2017-08-15 10:14:07 +0300

    Font test

--- Resources/views/prints/partials/style.blade.php
+++ Resources/views/prints/partials/style.blade.php
@@ -5,7 +5,7 @@
     }
 
     *{
-        font-size:12px;
+        font-size:11px;
         /* line-height:24px;*/
         /*font-family:Serif,'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;*/
         font-family: 'EB Garamond', serif;
