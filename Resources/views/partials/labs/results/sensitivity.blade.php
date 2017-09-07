511566d081b0d3f78b1139054c8b8c99cd3b70a9
BravoGidi <bkiptoo@collabmed.com>
2017-09-07 20:33:45 +0300

    sensitivity output

--- Resources/views/partials/labs/results/sensitivity.blade.php
+++ Resources/views/partials/labs/results/sensitivity.blade.php
@@ -12,11 +12,15 @@
             <th>Drug</th>
             <th>Sensitivity</th>
         </tr>
+        <?php try{ ?>
         @foreach($item->results->sensitivity_results as $stvt)
             <tr>
                 <td>{{$stvt->drug->name}}</td>
                 <td>{{$stvt->sensitivity}}</td>
             </tr>
         @endforeach
+        <?php }catch (\Exception $e){
+            //
+        } ?>
     </table>
 </td>
