aea8b095c7480aa2eeeae37f5030b97be4b3f886
David Ngugi <ndavidngugi@gmail.com>
2017-09-19 16:02:03 +0300

    Fix MassAssignment Visit Bug in Evaluation

--- Entities/Vitals.php
+++ Entities/Vitals.php
@@ -59,6 +59,7 @@ class Vitals extends Model {
 
     public $primaryKey = 'visit';
     public $table = 'evaluation_vitals';
+    public $fillable = ["visit","weight","height","bp_systolic","bp_diastolic","pulse","respiration","temperature","temperature_location","oxygen","waist","hip","blood_sugar","blood_sugar_units","allergies","chronic_illnesses","nurse_notes","user"];
 
     public function visits() {
         return $this->belongsTo(Visit::class, 'visit');
