830502c549cc6424ce706d720e892bfb26109f5f
Samuel Dervis <samueldervis@gmail.com>
2017-10-12 14:28:21 +0300

    ECEH-50 Update

--- Assets/js/doctor-prescriptions.js
+++ Assets/js/doctor-prescriptions.js
@@ -12,10 +12,6 @@ $(function () {
         save_prescription();
     });
 
-    function auto_calculate() {
-
-    }
-
     function map_select2(i) {
         //$('#addr' + i + ' select').select2({
         $('#item_' + i).select2({
@@ -67,10 +63,128 @@ $(function () {
             success: function () {
                 $('table#prescribed_drugs').dataTable().api().ajax.reload();
                 $('#prescription_form').trigger("reset");
+                alertify.success("Prescription saved");
             },
             error: function () {
                 alertify.error('<i class="fa fa-check-warning"></i> An error occured prescribing drug');
             }
         });
+    }
+
+    function getFrequency() {
+        var v = $prescriptionForm.find('[name=method]').val();
+        switch (v) {
+            case '1':
+                return 2;
+            case '2':
+                return 3;
+            case '3':
+                return 4;
+            case '4':
+            case '5':
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+    function getDuration() {
+        var duration = $prescriptionForm.find('[name=duration]').val();
+        var measure = $prescriptionForm.find('[name=time_measure]').val();
+        switch (measure) {
+            case '1':
+                $v = 1;
+                break;
+            case '2':
+                $v = 7;
+                break;
+            case '3':
+                $v = 30;
+                break;
+            case '4':
+                $v = 365;
+                break;
+            default:
+                $v = 0;
+                break;
+        }
+        return parseInt(duration) * $v;
+    }
+
+    function get_the_units() {
+        var dose = $prescriptionForm.find('[name=take]').val();
+        var frequency = getFrequency();
+        var duration = getDuration();
+        var mine = parseInt(dose) * duration * frequency;
+        if (mine) {
+            $prescriptionForm.find('[name=quantity]').val(mine);
+        }
+    }
+
+    var $prescriptionForm = $('#prescription_form');
+    $prescriptionForm.find('[name=method],[name=time_measure]').change(function () {
+        get_the_units();
+    });
+    $prescriptionForm.find('[name=take],[name=duration]').keyup(function () {
+        get_the_units();
+    });
+
+    // OTHERS
+    var $theForm = $('#myModal');
+    $theForm.find('[name=method],[name=time_measure]').change(function () {
+        get_the_units2();
+    });
+    $theForm.find('[name=take],[name=duration]').keyup(function () {
+        get_the_units2();
+    });
+
+    function getFrequency2() {
+        var v = $theForm.find('[name=method]').val();
+        switch (v) {
+            case '1':
+                return 2;
+            case '2':
+                return 3;
+            case '3':
+                return 4;
+            case '4':
+            case '5':
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+    function getDuration2() {
+        var duration = $theForm.find('[name=duration]').val();
+        var measure = $theForm.find('[name=time_measure]').val();
+        switch (measure) {
+            case '1':
+                $v = 1;
+                break;
+            case '2':
+                $v = 7;
+                break;
+            case '3':
+                $v = 30;
+                break;
+            case '4':
+                $v = 365;
+                break;
+            default:
+                $v = 0;
+                break;
+        }
+        return parseInt(duration) * $v;
+    }
+
+    function get_the_units2() {
+        var dose = $theForm.find('[name=take]').val();
+        var frequency = getFrequency2();
+        var duration = getDuration2();
+        var mine = parseInt(dose) * duration * frequency;
+        if (mine) {
+            $theForm.find('[name=quantity]').val(mine);
+        }
     }
 });
