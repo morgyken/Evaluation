d38fae4a252e237b5a74cb638c493d39db7c0576
Samuel Dervis <samueldervis@gmail.com>
2017-10-13 19:03:22 +0300

    Updates

--- Entities/Investigations.php
+++ Entities/Investigations.php
@@ -53,70 +53,93 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|\Ignite\Evaluation\Entities\Investigations whereVisit($value)
  * @mixin \Eloquent
  */
-class Investigations extends Model {
+class Investigations extends Model
+{
 
     protected $table = 'evaluation_investigations';
     protected $guarded = [];
     protected $appends = ['is_paid'];
 
-    public function getHasResultAttribute() {
+    public function getHasResultAttribute()
+    {
         return count($this->results);
     }
 
-    public function getPriceAttribute($value) {
+    public function getPriceAttribute($value)
+    {
         return intval($value);
     }
 
-    public function getIsPaidAttribute() {
-        return (bool) $this->payments;
+    public function getIsPaidAttribute()
+    {
+        return (bool)$this->payments;
     }
 
-    public function scopeLaboratory($query) {
+    public function scopeLaboratory($query)
+    {
         return $query->where('type', 'laboratory');
     }
 
-    public function scopeDiagnosis($query) {
+    public function scopeDiagnosis($query)
+    {
         return $query->where('type', 'diagnosis');
     }
 
-    public function scopeTreatment($query) {
+    public function scopeTreatment($query)
+    {
         return $query->where('type', 'treatment');
     }
 
-    public function visits() {
+    public function visits()
+    {
         return $this->belongsTo(Visit::class, 'visit');
     }
 
-    public function procedures() {
+    public function procedures()
+    {
         return $this->hasOne(Procedures::class, 'id', 'procedure');
     }
 
-    public function doctors() {
+    public function doctors()
+    {
         return $this->belongsTo(User::class, 'user');
     }
 
-    public function p() {
+    public function p()
+    {
         return $this->belongsTo(Procedures::class, 'procedure');
     }
 
-    public function results() {
+    public function results()
+    {
         return $this->hasOne(InvestigationResult::class, 'investigation');
     }
 
-    public function payments() {
+    public function payments()
+    {
         return $this->hasOne(EvaluationPaymentsDetails::class, 'investigation');
     }
 
-    public function getCashAttribute() {
+    public function getCashAttribute()
+    {
 
     }
 
-    public function getPesaAttribute() {
+    public function getPesaAttribute()
+    {
         return 'Ksh ' . number_format($this->price, 2);
     }
 
-    public function removed_bills() {
+    public function removed_bills()
+    {
         return $this->hasOne(\Ignite\Finance\Entities\RemovedBills::class, 'investigation');
     }
 
+    public function getTypeAttribute($value)
+    {
+        if ($value == 'treatment.nurse') {
+            return 'Nurse';
+        }
+        return ucfirst($value);
+    }
 }
