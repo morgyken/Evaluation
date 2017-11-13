60ae5ebe0b4a9e88d921ba86f18bd118cd0012be
Samuel Dervis <samueldervis@gmail.com>
2017-11-13 18:24:39 +0300

    Try catch

--- Resources/views/partials/labs/preload.blade.php
+++ Resources/views/partials/labs/preload.blade.php
@@ -10,8 +10,8 @@ $loaded = get_lab_template($item->procedures->id);
 $headers = array();
 $t_array = array();
 $with_headers = array();
-foreach ($loaded as $l){
-    if (!empty($l->header)){
+foreach ($loaded as $l) {
+    if (!empty($l->header)) {
         $headers[] = $l->header;
     }
 }
@@ -19,74 +19,81 @@ $other_tests = get_lab_template($item->procedures->id);
 ?>
 <table class="table table-condensed table-striped">
     <tr>
-        @foreach(array_unique($headers) as $header)
+    @foreach(array_unique($headers) as $header)
         <tr>
             <th colspan="2">{{$header->name}}</th>
         </tr>
         <?php $tests = get_title_procedures($item->procedures->id, $header->id); ?>
-            @foreach($tests as $test)
+        @foreach($tests as $test)
             <?php
+            try{
             $t_array[] = $test->subtests->id;
             $with_headers[] = $test->subtests->id;
             ?>
             @if($test->subtests->sensitivity)
-            <?php $s_item =$test->subtests; ?>
+                <?php $s_item = $test->subtests; ?>
                 @include('evaluation::partials.labs.sensitivity')
             @else
-                    <tr>
-                        <td>
-                            {{strtoupper($test->subtests->name)}}
-                            <input type="hidden" name="item{{$item->id}}" value="{{$item->id}}" />
-                            <input type="hidden" name="test{{$item->id}}[]" value="{{$test->subtest}}" />
-                        </td>
-                        <td>
-                            <?php
-                                try{
-                                    $type = $test->subtests->this_test->lab_result_type;
-                                }catch (\Exception $e){
+                <tr>
+                    <td>
+                        {{strtoupper($test->subtests->name)}}
+                        <input type="hidden" name="item{{$item->id}}" value="{{$item->id}}"/>
+                        <input type="hidden" name="test{{$item->id}}[]" value="{{$test->subtest}}"/>
+                    </td>
+                    <td>
+                        <?php
+                        try {
+                            $type = $test->subtests->this_test->lab_result_type;
+                        } catch (\Exception $e) {
 
-                                }
-                            ?>
-                            @include('evaluation::partials.labs.input_field')
-                        </td>
-                    </tr>
+                        }
+                        ?>
+                        @include('evaluation::partials.labs.input_field')
+                    </td>
+                </tr>
             @endif
-            @endforeach
+            <?php
+            }
+            catch (\Exception $e) {
+            }?>
         @endforeach
+    @endforeach
     <tr>
-        <th colspan="2"><hr/></th>
+        <th colspan="2">
+            <hr/>
+        </th>
     </tr>
     @foreach($other_tests as $_test)
         @if(!in_array($_test->subtests->id,$with_headers))
             <?php $t_array[] = $_test->subtests->id; ?>
             @if($_test->subtests->sensitivity)
                 <?php
-                $s_item =$_test->subtests;
+                $s_item = $_test->subtests;
                 ?>
                 @include('evaluation::partials.labs.sensitivity')
-             @else
-            <tr>
-                <td>
-                    {{$_test->subtests->name}}
-                    <input type="hidden" name="item{{$item->id}}" value="{{$item->id}}" />
-                </td>
-                <td>
-                    <?php
-                    if(!empty($_test->subtests->this_test)){
-                        $type = $_test->subtests->this_test->lab_result_type;
-                    }else{
-                        $type = null;
-                    }
-                    ?>
-                    @include('evaluation::partials.labs.input_field')
-                </td>
-            </tr>
+            @else
+                <tr>
+                    <td>
+                        {{$_test->subtests->name}}
+                        <input type="hidden" name="item{{$item->id}}" value="{{$item->id}}"/>
+                    </td>
+                    <td>
+                        <?php
+                        if (!empty($_test->subtests->this_test)) {
+                            $type = $_test->subtests->this_test->lab_result_type;
+                        } else {
+                            $type = null;
+                        }
+                        ?>
+                        @include('evaluation::partials.labs.input_field')
+                    </td>
+                </tr>
             @endif
         @endif
     @endforeach
-        <input type="hidden"  name="tests{{$item->id}}" value="{{json_encode($t_array)}}" />
-        <td colspan="2">
-            @include('evaluation::partials.labs.comment')
-        </td>
+    <input type="hidden" name="tests{{$item->id}}" value="{{json_encode($t_array)}}"/>
+    <td colspan="2">
+        @include('evaluation::partials.labs.comment')
+    </td>
     </tr>
 </table>
