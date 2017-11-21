60c772bcf589ccd9891f90559504978820982e1a
kisiara <kisiarafrancis@gmail.com>
2017-11-21 07:40:43 +0000

    editing the patient_prescriptions table to include a quantity column

--- Database/Migrations/2017_04_07_081435_create_patient_prescriptions_table.php
+++ Database/Migrations/2017_04_07_081435_create_patient_prescriptions_table.php
@@ -27,6 +27,7 @@ class CreatePatientPrescriptionsTable extends Migration {
             $column->boolean('status')->default(false);
             $column->boolean('allow_substitution')->default(false);
             $column->integer('time_measure')->unsigned()->default(1);
+            $column->integer('quantity');
             //$column->integer('type')->default(0); // 0 - Once only, 1- regular
             $column->timestamps();
 
